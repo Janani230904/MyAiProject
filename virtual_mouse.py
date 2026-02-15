e9437e8b32b57cff286a3c85e500651a4ec6f182
Janani230904 <janurgowda239@gmail.com>
2026-02-15 13:56:42 +0530

    Removed heavy environment files to fix push error

--- virtual_mouse.py
+++ virtual_mouse.py
@@ -1,25 +1,44 @@
-if hands:
-        # Get the list of landmarks and the hand type (left or right)
-        lmList = hands[0]['lmList']
-        
-        # 1. Coordinates for Index (8) and Middle (12) finger tips
-        x1, y1 = lmList[8][0], lmList[8][1]
-        x2, y2 = lmList[12][0], lmList[12][1]
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import pyautogui
+import numpy as np
 
-        # 2. Draw the rectangle and map coordinates (same as before)
+# Setup Camera and Detector
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1, detectionCon=0.8)
+screen_w, screen_h = pyautogui.size()
+
+while True:
+    success, img = cap.read()
+    if not success: break
+    img = cv2.flip(img, 1)
+
+    # --- THIS PART WAS MISSING OR MISPLACED ---
+    # This line creates the 'hands' variable
+    hands, img = detector.findHands(img, flipType=False)
+    # ------------------------------------------
+
+    if hands:
+        lmList = hands[0]['lmList']
+        x1, y1 = lmList[8][0], lmList[8][1] # Index Finger
+        x2, y2 = lmList[12][0], lmList[12][1] # Middle Finger
+
+        # Mapping to screen
         cv2.rectangle(img, (100, 100), (540, 380), (255, 0, 255), 2)
         mouse_x = np.interp(x1, (100, 540), (0, screen_w))
         mouse_y = np.interp(y1, (100, 380), (0, screen_h))
 
-        # 3. Move the mouse cursor
+        # Movement
         pyautogui.moveTo(mouse_x, mouse_y, _pause=False)
 
-        # 4. Check distance for CLICKING
-        # findDistance calculates the pixels between two points
+        # Clicking (Pinch distance)
         length, info, img = detector.findDistance(lmList[8][:2], lmList[12][:2], img)
-
-        # If the fingers are very close (less than 40 pixels), click!
         if length < 40:
-            cv2.circle(img, (info[4], info[5]), 15, (0, 255, 0), cv2.FILLED)
             pyautogui.click()
-            print("Click detected!")
+            cv2.circle(img, (info[4], info[5]), 15, (0, 255, 0), cv2.FILLED)
+
+    cv2.imshow("Virtual Mouse", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+cap.release()
+cv2.destroyAllWindows()
